9999b07bc05e67707dbb68bafcdfa298aebc03d5
AntoinePrv <AntoinePrv@users.noreply.github.com>
2022-08-24 10:17:29 +0200

    Add gnome/gsettings dark mode

---
+++ linux/gnome/dark-mode.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+readonly setting_name=("org.gnome.desktop.interface" "color-scheme")
+
+function dark_mode_get() {
+	case "$(gsettings get "${setting_name[@]}")" in
+		*dark*)
+			echo "dark";;
+		*light*)
+			echo "light";;
+	esac
+}
+
+function dark_mode_dark() {
+	gsettings set "${setting_name[@]}" "prefer-dark"
+}
+
+function dark_mode_light() {
+	gsettings set "${setting_name[@]}" "prefer-light"
+}
+
+function dark_mode_toogle() {
+	case "$(dark_mode_get)" in
+		*dark*)
+			dark_mode_light;;
+		*light*)
+			dark_mode_dark;;
+	esac
+}
+
+function dark_mode_listen() {
+	gsettings monitor "${setting_name[@]}" | while read line; do
+			case "${line}" in
+				*dark*)
+					"$@" "dark";;
+				*light*)
+					"$@" "light";;
+			esac
+		done
+}
+
+function dark_mode_base16_set() {
+	case "${1}" in
+		*dark*)
+			bash "${root}/scripts/base16-${dark}.sh";;
+		*light*)
+			bash "${root}/scripts/base16-${light}.sh";;
+	esac
+}
+
+
+function dark_mode_base16() {
+	local args=()
+	local root="${BASE16_DIR:-}"
+	local light="${BASE16_LIGHT_THEME:-one-light}"
+	local dark="${BASE16_DARK_THEME:-onedark}"
+	while [[ $# -gt 0 ]]; do
+		case "${1}" in
+			--root)
+				root="${2}"
+				shift 2;;
+			--light)
+				light="${2}"
+				shift 2;;
+			--dark)
+				dark="${2}"
+				shift 2;;
+			*)
+				args+=("${1}")
+				shift;;
+		esac
+	done
+	dark_mode_listen dark_mode_base16_set
+}
+
+function dark_mode_main() {
+	local -r args=("$@")
+	"dark_mode_${1}" "${args[@]:1}"
+}
+
+# Run the main when script is not being sourced
+if [[ "${BASH_SOURCE[0]}" = "${0}" ]] ; then
+
+	# Fail fast
+	set -o errexit
+	set -o pipefail
+	set -o nounset
+
+	dark_mode_main "$@"
+
+fi
